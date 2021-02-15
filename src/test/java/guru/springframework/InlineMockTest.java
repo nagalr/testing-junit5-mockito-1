27cb4624add0b9e7e8b28aa5ecc14e8af61478ca
Ronnen Nagal <nagalr@gmail.com>
2021-02-15 15:36:24 +0200

    Add Mockito-related dependencies

---
+++ src/test/java/guru/springframework/InlineMockTest.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by ronnen on 15-Feb-2021
+ */
+
+
+public class InlineMockTest {
+    @Test
+    void testInlineMock() {
+//        Map mapMock = mock(Map.class);
+//
+//        assertEquals(mapMock.size(), 0);
+    }
+}
