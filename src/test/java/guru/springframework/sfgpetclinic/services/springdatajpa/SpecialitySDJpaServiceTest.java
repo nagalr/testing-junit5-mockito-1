eae63578883e3d741a8a67af93b04b51bc272120
Ronnen Nagal <nagalr@gmail.com>
2021-02-16 14:24:35 +0200

    Add 'findAllTest' method to SpecialitySDJpaServiceTest.java

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -20,39 +21,75 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
 
+    // definition of a repository Mock
     @Mock
     SpecialtyRepository specialtyRepository;
 
+    // definition of a service Mock
+    // the repository Mock will be injected into it
     @InjectMocks
     SpecialitySDJpaService specialitySDJpaService;
 
     @Test
     void deleteTest() {
-        specialitySDJpaService.delete(new Speciality());
+
+        // define a Speciality object for testing
+        Speciality speciality = new Speciality();
+
+        // define operation for the Speciality-Service,
+        // the operation will be done by the repository
+        specialitySDJpaService.delete(speciality);
+
+        // verify that the repository was called once
+        // on the 'speciality' object.
+        verify(specialtyRepository, times(1)).delete(speciality);
     }
 
     @Test
     void deleteByIdTest() {
         specialitySDJpaService.deleteById(1L);
-
-        verify(specialtyRepository,never()).deleteById(2L);
+        verify(specialtyRepository,times(1)).deleteById(1L);
     }
 
     @Test
     void findByIdTest() {
+
+        // internal objects for testing
         Speciality returning = new Speciality();
 
         // tell the repository Mock what to do on 'findById' call
-        // the repository Mock is making to call the
+        // the repository Mock is making the call to Service object
         when(specialtyRepository.findById(1L)).thenReturn(Optional.of(returning));
 
-        // do what we defined
+        // the service is expecting 'findById'
+        // in practice, the repository Mock is executing the call, as we defined a line above
         returning = specialitySDJpaService.findById(1L);
 
-        // checking the return value
+        // checking the return value is not null
         assertThat(returning).isNotNull();
 
         // verify that 'findById' returned the speciality object
         verify(specialtyRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void findAllTest() {
+
+        // define a reference to Speciality Set for testing
+        // initialize to 'null' in-purpose, we check later if its not null
+        Set<Speciality> specialities = null;
+
+        // define what the service should do when called on 'findAll'
+        when(specialitySDJpaService.findAll()).thenReturn(specialities);
+
+        // execute the 'findAll' call
+        specialities = specialitySDJpaService.findAll();
+
+        // verify that 'specialtyRepository' was called once on 'findAll'
+        verify(specialtyRepository, times(1)).findAll();
+
+        // verify that 'specialities' is not null
+        assertThat(specialities).isNotNull();
+
     }
 }
